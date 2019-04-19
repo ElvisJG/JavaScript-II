097a986361ab4029291bb0ef77f1bac5a2469b6b
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-04-18 20:40:00 -0400

    >.>

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,17 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function batman() {
+  let secretIdentity = 'Bruce Wayne';
+  function joker() {
+    console.log(`Why hello there Batman, or should i say... ${secretIdentity}`);
+  }
+  return joker;
+}
+
+let gotham = batman();
+console.log(gotham());
+
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 2: Create a counter function ====
@@ -25,26 +36,23 @@ console.log(newCounter()); //5
 console.log(newCounter()); //6
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
 
+const counterFactory = () => {
   let count = 0;
-  return {
-    increment: () => count++,
-    decrement: () => count--
+
+  this.increment = () => {
+    return ++count;
+  };
+
+  this.decrement = () => {
+    return --count;
   };
 };
 
 const newestCounter = counterFactory();
-
-console.log(newestCounter());
 newestCounter.increment();
 newestCounter.increment();
-newestCounter.increment();
-console.log(newestCounter());
-console.log(newestCounter(increment));
-newestCounter.decrement();
-newestCounter.decrement();
-console.log(newestCounter());
+console.log(newestCounter);
